d4cfdecc0bc4cedea1fcd463b671d4b696e04bdc
bhargav kilaru <kilabhar@gmail.com>
2021-04-22 22:51:31 -0400

    changes related to emp form

--- src/components/EmpForm.js
+++ src/components/EmpForm.js
@@ -6,32 +6,79 @@ class EmpForm extends Component{
 
     constructor(props){
         super(props)
+
+        this.state = {
+			empId: '',
+			empName: '',
+			email: '',
+            address:''
+		}
     }
+
+    handleEmpIdChange = event => {
+		this.setState({
+			empId: event.target.value
+		})
+	}
+
+    handleEmpNameChange = event => {
+		this.setState({
+			empName: event.target.value
+		})
+	}
+
+    handleEmailChange = event => {
+		this.setState({
+			email: event.target.value
+		})
+	}
+
+    handleAddressChange = event => {
+		this.setState({
+			address: event.target.value
+		})
+	}
+
+    handleSubmit = event => {
+		alert(`${this.state.empId} ${this.state.empName} ${this.state.email} ${this.state.address}`)
+		event.preventDefault()
+	}
+
+
 
 
     render(){
 
-        const { empForm } = this.props;
+       // const { empForm } = this.props;
+        const { empId, empName, email, address } = this.state
 
         return(
-            <div className="conatiner">
+            <div className="container">
                 <h1>Add New EMployee</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}> 
                     
                     <div className="form-inline">
-                        <label for="emp Id" className="mr-sm-2">Emp Id:</label>
-                        <input type="empId" className="form-control mb-2 mr-sm-2" placeholder="Enter emp Id" id="emp Id"/>
+                        <label for="empId" className="mr-sm-2">Emp Id:</label>
+                        <input type="text" className="form-control mb-2 mr-sm-2" value={empId} 
+                        onChange={this.handleEmpIdChange}
+                        placeholder="Enter emp Id" id="empId"/>
                         &nbsp;
-                        <label for="emp Name" className="mr-sm-2">Emp Name:</label>
-                        <input type="empName" className="form-control mb-2 mr-sm-2" placeholder="Enter emp Name" id="emp Name"/>
+                        <label for="empName" className="mr-sm-2">Emp Name:</label>
+                        <input type="text" className="form-control mb-2 mr-sm-2" value={empName}
+                         onChange={this.handleEmpNameChange}
+                        placeholder="Enter emp Name" id="empName"/>
                     </div>
 
                     <div className="form-inline">
                         <label for="email" className="mr-sm-2">Email Id:</label>
-                        <input type="email" className="form-control mb-2 mr-sm-2" placeholder="Enter email" id="email"/>
+                        <input type="email" className="form-control mb-2 mr-sm-2" value={email} 
+                         onChange={this.handleEmailChange}
+                        placeholder="Enter email" id="email"/>
                         &nbsp;
-                        <label for="pwd" className="mr-sm-2">Password:</label>
-                        <input type="password" className="form-control mb-2 mr-sm-2" placeholder="Enter password" id="pwd"/>
+                        <label for="address" className="mr-sm-2">Address:</label>
+                        <input type="text" className="form-control mb-2 mr-sm-2"  value={address}
+                         onChange={this.handleAddressChange}
+                           placeholder="Enter Address" id="address"/>
                     </div>
                     <div className="form-group form-check">
                         <label className="form-check-label">
